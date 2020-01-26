d2ac222c757c003acc8c2a21d6c767b7bed32164
jOkhrimenko <56116052+jOkhrimenko@users.noreply.github.com>
2020-01-26 22:46:14 +0200

    Add files via upload

---
+++ gra/Gra.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.Font = new System.Drawing.Font("Californian FB", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelabel.Location = new System.Drawing.Point(587, 18);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(46, 51);
+            this.scorelabel.TabIndex = 0;
+            this.scorelabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp4.Resource1.Background;
+            this.ClientSize = new System.Drawing.Size(744, 506);
+            this.Controls.Add(this.scorelabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scorelabel;
+    }
+}
+
---
+++ gra/Gra.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap HandlerTexure = Resource1.Handler,
+            TargetTexture = Resource1.Target;
+        private Point _targetPosition = new Point (300, 300);
+        private Point _direction = Point.Empty;
+        private int _score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | //чтобы не мелькал круг
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e) //таймер 
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 1000);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           Graphics g = e.Graphics;
+
+            var localPosition = this.PointToClient(Cursor.Position); //круг за курсором 
+
+            _targetPosition.X += _direction.X * 10;
+            _targetPosition.Y += _direction.Y * 10;
+
+            if(_targetPosition.X < 0 || _targetPosition.X > 500)
+            {
+                _direction.X *= -1;
+            }
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+                _direction.Y *= -1;
+            }
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+
+            if (distance < 20)
+            {
+                AddScore(1);
+            }
+
+
+
+            var handlerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            g.DrawImage(TargetTexture, handlerRect);
+            g.DrawImage(HandlerTexure, targetRect);
+        }
+
+        private void AddScore (int score) //сами создаем для счета очков 
+        {
+            _score += score;
+            scorelabel.Text = _score.ToString();
+        }
+
+    }
+}
